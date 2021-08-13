3f1d9e5f7c71056b6062eeff18d0b57d724333c8
Tanja <tanjamilisavljevic7@gmail.com>
2021-08-13 09:06:51 +0200

    Initialize repository

---
+++ Starter-pack/index.php
@@ -0,0 +1,41 @@
+<?php
+
+// Require the correct variable type to be used (no auto-converting)
+declare(strict_types = 1);
+
+// Show errors so we get helpful information
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+session_start();
+
+function whatIsHappening() {
+    echo '<h2>$_GET</h2>';
+    var_dump($_GET);
+    echo '<h2>$_POST</h2>';
+    echo('<pre>');
+    var_dump($_POST);
+    echo('</pre>');
+    echo '<h2>$_COOKIE</h2>';
+    var_dump($_COOKIE);
+    echo '<h2>$_SESSION</h2>';
+    var_dump($_SESSION);
+}
+
+whatIsHappening();
+
+
+// Load your classes
+require_once 'classes/Data.php';
+require_once 'classes/LanguageGame.php';
+// require_once 'classes/Player.php'; // Only needed for extra's
+require_once 'classes/Word.php';
+
+// Start the game
+// Don't change anything in this file
+// The LanguageGame class will be your starting point
+$game = new LanguageGame();
+$game->run();
+
+require 'view.php';
---
+++ Starter-pack/view.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Game</title>
+</head>
+<body>
+	<!-- TODO: add a form for the user to play the game -->
+    <form>
+        <p class="randomChoice"> The word to translate: </p>
+        <label for="userChoice">Your guess:</label> <br>
+        <input type="text" id="userChoice" name="userChoice"><br>
+
+        <button type="submit" name="submit">Go</button>
+
+    </form>
+</body>
+</html>
